ed32753a134f7255db80ffae78b2f1fc449bb536
Jonathan Baker <jonjonbak@gmail.com>
2023-04-21 16:42:24 -0700

    Modified the last exercise, added a prompter

--- lilMod.py
+++ lilMod.py
@@ -1,11 +1,25 @@
 def houseArea(l,w):
-    return l*w
+    print(f"The area is {l*w}")
+    # return l*w
 
-def circCirc():
-    #2 pi r
+def circCirc(r):
     from math import pi
-    r=input("What is the radius?:")
-    print(f"The circumference is {float(r)*2*pi}")
+    print(f"The circumference is {r*2*pi}")
+    # return (float(r)*2*pi)
 
 
-
+def prompter():
+    #Assuming valid inputs
+    what_do=input("Choose to calculate house area (ha) or circle circumference (cc): ")
+    if what_do=="ha":
+        l=input("Enter length: ")
+        w=input("Enter width: ")
+        try:
+            houseArea(int(l),int(w))
+        except:
+            houseArea(float(l),float(w))
+    elif what_do=="cc":
+        r=input("Enter radius: ")
+        circCirc(float(r)) #pi means this is always gonna be floats
+    else:
+        print("Bad input")
