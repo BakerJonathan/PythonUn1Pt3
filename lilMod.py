96d5430f15b7d88d1f9d67bbe772e53c16a9cd72
Jonathan Baker <jonjonbak@gmail.com>
2023-04-21 16:15:37 -0700

    first commit

---
+++ lilMod.py
@@ -0,0 +1,11 @@
+def houseArea(l,w):
+    return l*w
+
+def circCirc():
+    #2 pi r
+    from math import pi
+    r=input("What is the radius?:")
+    print(f"The circumference is {float(r)*2*pi}")
+
+
+
